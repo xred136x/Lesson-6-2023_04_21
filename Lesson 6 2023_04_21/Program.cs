403d0a83956273a65efffc56a07bbd4746cd604e
С - 3 < - 3@DESKTOP-E0FTS21>
2023-04-21 22:04:49 +0300

    Добавьте файлы проекта.

---
+++ Lesson 6 2023_04_21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Делегаты
+
+namespace Lesson_6_2023_04_21
+{
+    delegate void myDel(string _text);
+    internal class Program
+    {
+        static void Print(string _text)
+        {
+            Console.WriteLine(_text);
+        } 
+        static void WriteToFile(string _text)
+        {
+            string _name = "output.txt";
+            var sw = new StreamWriter(_name, true);
+            sw.WriteLine(_text);
+            sw.Close();
+           /* using (var sw01 = new StreamWriter(_name, true))
+                 sw01.WriteLine(_text);*/
+        }
+        static void Main(string[] args)
+        {
+            myDel output;
+            output = Print;
+            output += WriteToFile;
+            /*output($"Hello! {DateTime.Now}");
+            output.Invoke($"Hello! {DateTime.Now.ToString("HH:mm:ss.fff")}");*/
+            output.Invoke(null);
+            output(null);
+            Console.ReadKey();
+        }
+    }
+}
